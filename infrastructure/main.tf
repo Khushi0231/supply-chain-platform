dbf4741369e1d6617e90029210bcc45f00cc30e0
Khushi0231 <149259513+Khushi0231@users.noreply.github.com>
2025-07-18 21:36:05 +0000

    Added S3 and DynamoDB Terraform config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,23 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_lambda_function" "file_processor" {
+  function_name = "file_processor"
+  role          = "<PASTE_IAM_ROLE_ARN_FROM_PERSON_B>"
+  handler       = "file_processor.lambda_handler"
+  runtime       = "python3.9"
+  filename      = "lambda/file_processor.zip"
+  source_code_hash = filebase64sha256("lambda/file_processor.zip")
 }
-resource "aws_s3_bucket" "data_bucket" {
-  bucket = "supply-chain-data-${random_string.suffix.result}"
-}
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-resource "aws_dynamodb_table" "inventory" {
-  name           = "Inventory"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "product_id"
-  attribute {
-    name = "product_id"
-    type = "S"
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.data_bucket.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.file_processor.arn
+    events              = ["s3:ObjectCreated:*"]
   }
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.data_bucket.arn
 }
